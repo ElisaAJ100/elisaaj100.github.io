7c76bce5abd01420dae3b3600040738d91202e3f
ElisaAJ100 <eisaaj.es@gmail.com>
2022-06-21 22:40:09 -0300

    w

--- javascript.js
+++ javascript.js
@@ -1,8 +1,15 @@
 
 function mostraHora() {
-    let titulo = document.querySelector('h2');
+    let titulo = document.querySelector('h1');
     let agora = new Date();
     titulo.textContent = agora.toLocaleDateString();
 }
 mostraHora();
 setInterval(mostraHora,1000);
+const titulos = document.querySelectorAll('h2');
+const topicos = new Array();
+titulos.forEach(titulo => {
+    topicos.push(titulo.textContent);
+});
+paragrafo = document.querySelector('p');
+paragrafo.textContent = topicos
