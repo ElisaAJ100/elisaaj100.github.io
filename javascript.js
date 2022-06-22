4896c78d0ca8adb45d3209cd6290c02b6c2ee042
ElisaAJ100 <eisaaj.es@gmail.com>
2022-06-21 23:12:33 -0300

    w

--- javascript.js
+++ javascript.js
@@ -3,9 +3,10 @@ const topicos = new Array();
 subtitulos.forEach(titulo => {
     topicos.push(titulo.textContent);
 });
-L = document.querySelector('ol');
+ListaCriada = document.querySelector('ol');
 topicos.forEach(topico => {
     const item = document.createElement('li');
     item.textContent = topico;
-    L.append(item);
+    ListaCriada.append(item);
 });
+S
