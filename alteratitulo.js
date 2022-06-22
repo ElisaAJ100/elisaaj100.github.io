9e3b6139fc269ca17dc505702084024f34c1a6a4
ElisaAJ100 <eisaaj.es@gmail.com>
2022-06-22 15:53:23 -0300

    c

--- alteratitulo.js
+++ alteratitulo.js
@@ -1,29 +1,29 @@
-if(document.getElement('h1')){
+if(document.getElement('h1') == true){
     const elemento = 'h1';
     alert('h1 selecionado');
 }
 else{
-    if(document.getElement('h2')){
+    if(document.getElement('h2') == true){
         const elemento = 'h2';
         alert('h2 selecionado');
     }
     else{
-        if(document.getElement('h3')){
+        if(document.getElement('h3') == true){
             const elemento = 'h3';
             alert('h3 selecionado');
         }
         else{
-            if(document.getElement('h4')){
+            if(document.getElement('h4') == true){
                 const elemento = 'h4';
                 alert('h4 selecionado');
             }
             else{
-                if(document.getElement('h5')){
+                if(document.getElement('h5') == true){
                     const elemento = 'h5';
                     alert('h5 selecionado');
                 }
                 else{
-                    if(document.getElement('h6')){
+                    if(document.getElement('h6') == true){
                         const elemento = 'h6';
                         alert('h6 selecionado');
                     }
