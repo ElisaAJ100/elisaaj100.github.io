30d677bdf043f5a25677a189ab1113f8c145ff81
ElisaAJ100 <eisaaj.es@gmail.com>
2022-06-22 15:51:20 -0300

    s

--- alteratitulo.js
+++ alteratitulo.js
@@ -1,29 +1,29 @@
-if(document.getElementById('h1')){
+if(document.getElement('h1')){
     const elemento = 'h1';
     alert('h1 selecionado');
 }
 else{
-    if(document.getElementById('h2')){
+    if(document.getElement('h2')){
         const elemento = 'h2';
         alert('h2 selecionado');
     }
     else{
-        if(document.getElementById('h3')){
+        if(document.getElement('h3')){
             const elemento = 'h3';
             alert('h3 selecionado');
         }
         else{
-            if(document.getElementById('h4')){
+            if(document.getElement('h4')){
                 const elemento = 'h4';
                 alert('h4 selecionado');
             }
             else{
-                if(document.getElementById('h5')){
+                if(document.getElement('h5')){
                     const elemento = 'h5';
                     alert('h5 selecionado');
                 }
                 else{
-                    if(document.getElementById('h6')){
+                    if(document.getElement('h6')){
                         const elemento = 'h6';
                         alert('h6 selecionado');
                     }
