129f49963a17a4e9a10fd9a96cc07f7c82cec2a6
ElisaAJ100 <eisaaj.es@gmail.com>
2022-06-22 18:39:10 -0300

    s

--- alteratitulo.js
+++ alteratitulo.js
@@ -1,42 +1,37 @@
-document.addEventListener('DOMContentLoaded', ComandaTodas);//Depois de documento atml carredo ele executará função comanda todas
-
-
-function ComandaTodas() {
-    if(document.querySelector('h1') !== null){
-        const elemento = 'h1';
-        alert('h1 selecionado');
-    }//fim if com h1
+if(document.querySelector('h1') !== null){
+    const elemento = 'h1';
+    alert('h1 selecionado');
+}//fim if com h1
+else{
+    if(document.querySelector('h2') !== null){
+        const elemento = 'h2';
+        alert('h2 selecionado');
+    }//fim if com h2
     else{
-        if(document.querySelector('h2') !== null){
-            const elemento = 'h2';
-            alert('h2 selecionado');
-        }//fim if com h2
+        if(document.querySelector('h3') !== null){
+            const elemento = 'h3';
+            alert('h3 selecionado');
+        }//fim if com h3
         else{
-            if(document.querySelector('h3') !== null){
-                const elemento = 'h3';
-                alert('h3 selecionado');
-            }//fim if com h3
+            if(document.querySelector('h4') !== null){
+                const elemento = 'h4';
+                alert('h4 selecionado');
+            }//fim if com h4
             else{
-                if(document.querySelector('h4') !== null){
-                    const elemento = 'h4';
-                    alert('h4 selecionado');
-                }//fim if com h4
+                if(document.querySelector('h5') !== null){
+                    const elemento = 'h5';
+                    alert('h5 selecionado');
+                }//fim if com h5
                 else{
-                    if(document.querySelector('h5') !== null){
-                        const elemento = 'h5';
-                        alert('h5 selecionado');
-                    }//fim if com h5
-                    else{
-                        if(document.querySelector('h6') !== null){
-                            const elemento = 'h6';
-                            alert('h6 selecionado');
-                        } // fim if com h6
-                    }//fim else
+                    if(document.querySelector('h6') !== null){
+                        const elemento = 'h6';
+                        alert('h6 selecionado');
+                    } // fim if com h6
                 }//fim else
             }//fim else
         }//fim else
     }//fim else
-}// fim função ComandaTodas()
+}//fim else
 
 function AlmentarTamanhoTexto(elemento) {
     elementoSelecionado = document.querySelector(elemento);
@@ -52,8 +47,8 @@ function InserirBotao() {
     div = document.createElement('div');
     div.style.backgroundColor = 'red';
     div.innerHTML = `
-    <button onclick="AlmentarTamanhoTexto()">+</button>
-    <button onclick="DiminuirTamanhoTexto()">-</button>
+    <button onclick='AlmentarTamanhoTexto()'>+</button>
+    <button onclick='DiminuirTamanhoTexto()'>-</button>
     `;
-    document.body.prepend(div);
+    document.body.prepend(elemento);
 }//fim função que inseri botões
