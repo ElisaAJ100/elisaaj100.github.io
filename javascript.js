44dda3817c22274f2d91aa3e04d6e8a04d71faa4
ElisaAJ100 <eisaaj.es@gmail.com>
2022-06-22 11:43:09 -0300

    agora vai

--- javascript.js
+++ javascript.js
@@ -1,12 +1,18 @@
-const subtitulos = document.querySelectorAll('h2');
+const subtitulos = document.querySelectorAll('h3');
 const topicos = new Array();
 subtitulos.forEach(titulo => {
     topicos.push(titulo.textContent);
+    const Name = document.createElement("a");
+    Name.setAttribute("name", element.textContent);
+	element.append(arco);
 });
 ListaCriada = document.querySelector('ol');
 topicos.forEach(topico => {
-    const centro = document.createElement('center')
     const item = document.createElement('li');
+    const linkelemento = document.createElement('a');
     item.textContent = topico;
     ListaCriada.append(item);
+    item.append(linkelemento);
+    linkelemento.setAttribute('href', `#${topico}`);
+    linkelemento.textContent = topico;
 });
